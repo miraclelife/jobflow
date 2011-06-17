838526aea6329943f386b4cf60c6cddec99f8c3e
Fiachra O'Donoghue <fodonoghue@gmail.com>
2011-06-17 12:04:22 +0000

    data error fixed, x range for groups added, attempting to access parent to duplicate vertical super-stack for each year

--- job_flow.js
+++ job_flow.js
@@ -1,11 +1,16 @@
 var w = 800,
     h = 600,
-    p = 20,
-    total = 0;
+    p = 20,  // padding
+    bw = 20, // bar width
+    total = 0,
+    start_year = 2006,
+    end_year = 2009,
+    year_range = d3.range(start_year, (end_year + 1));
     
 var data = [0,205724,0,5616,156041,294469,73080,132057,51191,12345,79154,90874];
 var datasum = data.reduce(function(a, b) { return a+b; }); 
-var y = d3.scale.linear().domain([0, datasum]).range([1, h]);
+var y = d3.scale.linear().domain([0, datasum]).range([1, h - p * 2]);
+var x = d3.scale.ordinal().domain(year_range).rangeBands([(0 + p), (w - p)]);
 
 var vis = d3.select("body")
     .append("svg:svg")
@@ -13,11 +18,38 @@ var vis = d3.select("body")
     .attr("width", w)
     .attr("height", h);
     
-vis.selectAll("rect")
+var yeargroup = vis.selectAll("g")
+    .data(year_range)
+    .enter().append("svg:g")
+    .attr("class", "yearbar")
+    .attr("x", function(d) { return x(d); })
+    .attr("y", p);
+    
+yeargroup.selectAll("rect")
     .data(data)
     .enter().append("svg:rect")
-    .attr("x", 20)
-    .attr("y", function(d, i) { total += y(d); return total - y(d) + i; })
-    .attr("width", 14)
+    .attr("x", p)
+    .attr("y", function(d, i) { total += y(d); return total - y(d) + i + p; })
+    .attr("width", bw)
     .attr("height", function(d) { return y(d); })
-    .on("mouseover", function() { d3.select(this).attr("width", 30); });
+    .attr("fill", function(d) { return d === 0 ? "red" : "steelblue"; })
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout);
+
+function mouseover(d, i) {
+    d3.select(this)
+        .attr("opacity", 0.5);
+}
+
+function mouseout(d, i) {
+    d3.select(this)
+        .attr("opacity", 1);
+}
+
+function cross(a) {
+  return function(d) {
+    var c = [];
+    for (var i = 0, n = a.length; i < n; i++) c.push({x: d, y: a[i]});
+    return c;
+  };
+}
