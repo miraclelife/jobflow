926d9ded74b1a8d736b8a6e975ced048f2543fb1
fod <fodonoghue@gmail.com>
2011-06-17 01:51:05 +0100

    vertically aligning bars

--- job_flow.js
+++ job_flow.js
@@ -1,9 +1,11 @@
 var w = 800,
     h = 800,
-    p = 20;
+p = 20,
+total = 0;
     
-var data = [0,205724,0,5616,156041,294469,73080,132057,51191,12345,79154,90874];
-var x = d3.scale.linear().domain([d3.min(data), d3.max(data)]).range([1, 750]);
+var data = [5000,205724,50000,5616,156041,294469,73080,132057,51191,12345,79154,90874];
+var datasum = data.reduce(function(a, b) { return a+b; }) 
+var y = d3.scale.linear().domain([0, datasum]).range([1, 750]);
 
 var vis = d3.select("body")
     .append("svg:svg")
@@ -14,7 +16,8 @@ var vis = d3.select("body")
 vis.selectAll("rect")
     .data(data)
     .enter().append("svg:rect")
-    .attr("x", function(d, i) { return i * 15 + p;  })
-    .attr("y", function(d) { return h - x(d); })
+    .attr("x", 20)
+    .attr("y", function(d, i) { total += d; console.log(y(d));return y(total - d) +20 ; })
     .attr("width", 14)
-    .attr("height", function(d) { return x(d); });
+    .attr("height", function(d) { return y(d); })
+    .on("mouseover", function() { d3.select(this).attr("width", 30); });
